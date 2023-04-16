60895556c84f62f251be8f61d8854e42e48f63e0
sammy0804 <sami31rincon@gmail.com>
2023-04-16 00:05:55 -0500

    comic API

---
+++ Controllers/ComicsController.cs
@@ -0,0 +1,32 @@
+﻿using ComicsApi.Model;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace DominoesApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ComicsController : ControllerBase
+    {
+
+        [HttpGet]
+        public async Task<IActionResult> GetComics()
+        {
+            using var httpClient = new HttpClient();
+
+            HttpResponseMessage response;
+            do
+            {
+                var rand = new Random();
+                int randomComicNumber = rand.Next(1, 2501);
+                response = await httpClient.GetAsync($"https://xkcd.com/{randomComicNumber}/info.0.json");
+            } while (response.StatusCode != HttpStatusCode.OK);
+
+            string responseBody = await response.Content.ReadAsStringAsync();
+            var comic = JsonConvert.DeserializeObject<Comic>(responseBody);
+
+            return Ok(comic);
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,38 @@
+﻿namespace ComicsApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseCors(policy => policy
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .SetIsOriginAllowed(origin => true)
+                .AllowCredentials());
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}

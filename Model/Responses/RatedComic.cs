a2e8b85b88b1ff4beba5da115f3ddfdbd35687a8
sammy0804 <sami31rincon@gmail.com>
2023-04-16 02:31:20 -0500

    Created response and services

---
+++ Model/Responses/RatedComic.cs
@@ -0,0 +1,8 @@
+﻿namespace ComicsApi.Model.Responses
+{
+    public class RatedComic
+    {
+        public Comic Comic { get; set; }
+        public int Rate { get; set; }
+    }
+}
---
+++ Services/ComicsService.cs
@@ -0,0 +1,39 @@
+﻿using ComicsApi.Model;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace ComicsApi.Services
+{
+    public class ComicsService
+    {
+        public async Task<Comic> GetComics()
+        {
+            using var httpClient = new HttpClient();
+
+            HttpResponseMessage response;
+            do
+            {
+                var rand = new Random();
+                int randomComicNumber = rand.Next(1, 2501);
+                response = await httpClient.GetAsync($"https://xkcd.com/{randomComicNumber}/info.0.json");
+            } while (response.StatusCode != HttpStatusCode.OK);
+
+            string responseBody = await response.Content.ReadAsStringAsync();
+            var comic = JsonConvert.DeserializeObject<Comic>(responseBody);
+
+            return comic;
+        }
+
+        public async Task<Comic> GetComic(int id)
+        {
+            using var httpClient = new HttpClient();
+            HttpResponseMessage response;
+            response = await httpClient.GetAsync($"https://xkcd.com/{id}/info.0.json");
+            string responseBody = await response.Content.ReadAsStringAsync();
+            var comic = JsonConvert.DeserializeObject<Comic>(responseBody);
+
+            return comic;
+        }
+    }
+}
